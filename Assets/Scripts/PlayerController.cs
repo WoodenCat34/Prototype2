fb6f43ddefa17ffa6e6c3d9fa14ef97204b15583
WoodenCat34 <woodencat34@gmail.com>
2024-10-12 20:20:56 +0300

    Add: x movement player boundries

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,18 +7,30 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float verticalInput;
-    public float speed = 15f;
+    public float speed = 15.0f;
+    public float xRangeRight = 21.35f;
+    public float xRangeLeft = -20.63f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
-        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed); 
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+        if (transform.position.x > xRangeRight)
+        {
+            transform.position = new Vector3 (xRangeRight, transform.position.y, transform.position.z);
+            
+        }
+        if (transform.position.x < xRangeLeft)
+        {
+            transform.position = new Vector3(xRangeLeft, transform.position.y, transform.position.z);
+
+        }
     }
 }
